d54d957a20b2b4c1beb3c712e1f04aea4774fc18
haririthanyaa <128139401+haririthanyaa@users.noreply.github.com>
2023-04-05 11:49:43 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './home.css'
+function Home() {
+    return (
+      <div>
+        <center><h2>Home</h2>
+        <p>Welcome to our travel website!</p>
+        </center>
+      </div>
+    );
+  }
+  export default Home;
